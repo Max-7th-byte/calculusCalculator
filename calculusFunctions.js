b07e4c87e7dae678e4fed4ea8b841e4f67158b1c
Max-7th-byte <max.dubakov@gmail.com>
2020-02-29 07:55:27 +0100

    Fixed compareCalculations function

--- calculusFunctions.js
+++ calculusFunctions.js
@@ -1,72 +1,6 @@
 const extractXReg = /\s*([\+|-]*)\s*(\d+\/\d+|\d+\.\d+|\d+)*x\^*(\d+)*\s*/g;
 const extractNumberReg = /\s*([\+|-]*)\s*(\d+\/\d+|\d+\.\d+|\d+)\s*/g;
 
-const functionDatabase = [
-    '-2x+x^5-x^4-x^2-18',  '-2x^4+x^2-10',       '-x^3-x^2-11',
-    '-x^4+x^3-x^5+22',     '-2x^3+x^2-16',       '-2x^5+x^4+17',
-    '-2x^3-x^5-9',         '2x^2-2x^3+25',       '2x^2-x^3-x^4-x-12',
-    '2x-2x^3+x^2-32',      '-x^4-x^2+x^5-23',    '-x^2-x^4-x-17',
-    '-2x^2+x^4-28',        'x^4-x^2-x^5-17',     '-2x^2-x^4-24',
-    '-2x^4-x^3-15',        '-3x^4+x^3-27',       '2x^3-x^4-x^5-14',
-    '-x-x^3+x^2-16',       '-2x-2x^2+x^5-12',    'x^2-x^4-x^3+20',
-    '-x^4-x^3-13',         '2x^2-2x-x^4+15',     '-x^3-x^4+14',
-    '-2x^4+x^2-x^5-12',    '-x^4-x^3-20',        '2x^3-x^4+12',
-    '-x^4+x^2-x^3-18',     '-2x^2+x^4-14',       '-2x+x^3-x^2+x^4+21',
-    '-2x^2+x^5-30',        '-2x^5-x^4-8',        '-x^5-x^3-21',
-    '-3x^2+x^5-x^4-10',    '-2x^4+x^2-x^5-9',    '-x^4+x^3-x^2-8',
-    '2x^2-2x^4+13',        '-2x^2+x^4-20',       '-2x^4+x-x^2-x^3-26',
-    '-2x^2+x^3-21',        '-x^2-x^3-18',        '-2x^2-x^3-30',
-    '2x^4-x^2-20',         '-x^3+x^4-x^2-13',    '-2x^4+x^5-9',
-    '2x^5-x^2-x^4-x^3-18', 'x-x^5-x^3-2',        '-2x^4-x^3-19',
-    '2x^2+x^3-x^5-36',   '-x^2-x^3-x+14',        '-2x^2+x^4+x^3+29',
-    '-x^4+x^5-x^3-20',   '-2x^3+x^2+x^4-23',     '2x^5-2x^3+17',
-    '2x^4-x^2+18',       '-2x^2+x^3-24',         '-x^2-2x^3-21',
-    '-2x^5+x^2-x^4-x-7', '-x^3-2x^2+30',         '-3x^5+x^3-x^2-26',
-    '-2x^3+x^2-23',      '-x^4-2x^5-7',          '-3x^3+x^2-32',
-    '2x^5+x^2-16',       '2x^3-x-x^5-12',        '2x^4-2x^2-15',
-    '2x^4-x-x^3+8',      '2x^2-x^3-21',          '-2x^2+x^5-24',
-    '2x^4-2x^2-x^3-24',  '2x^4-2x^2-23',         '-2x^2+x^5+19',
-    '-2x^3+x^2-10',      '2x^3+x-x^2-26',        '-x^3-x^4-x^2-20',
-    '2x^5-2x^3-1',       '-x^2-x^5+x-4',         '2x^3-2x^5-x^2-16',
-    '-x^3-x^2+38',       'x^2-x+x^4-18',         '2x^4-x^2+x^3-5',
-    '-2x^2-x^3-6',       '-x^3-x^4-x^2+12',      '-3x^3+x^5+15',
-    '2x^4-x^5+24',       '-2x^4+x^3-x^5+30',     '-x^4-x^3-19',
-    '-x^2+x^3-13',       '-x^5-x^4+10',          '-x^3-x^4-12',
-    '-x-x^2-x^4-12',     'x^2+x^4+x+24',         '2x^5-x^2+20',
-    'x^4+x^2-x^3-20',    'x^2-2x^5-13',          '-x^2-2x^4-15', 
-    '-x^4+x^2+11',       '-2x^3+x^5-x^4-x^2-21', '2x^3-2x^4+x^5-26',
-    '-2x^3+x^2+x^5-32',  '-x^3-x^5-31',          '-x^2-x^5-19',
-    '-x^5-x^4-x-19',     '-2x^3-x^4+5',          'x^5-2x^2-15',
-    '-x-x^5-x^4-32',     '-2x^2+x-x^3+21',       '-2x^3+x^4-x^2-29',
-    '2x^3-x^5+x^4-x-12', '-2x^5+x^2-22',         '-2x-2x^4+x^3-23',
-    '-2x^3+x^2-27',      '-2x^3+x^2+x+x^5-29',   '2x^4-x^2-x^3+21',
-    '-3x^4+x^2-x^3+26',  '2x^5-x^2-x-x^3-3',     '-2x^2+x^3+9',
-    '-2x^5-x^2-8',       '-2x^2+x-x^3-37',       '-3x^3+x^5+x-25',
-    '-2x^2+x^3-x^4-38',  '-x^4-x-x^3-11',        '-x^3-x^4+34',
-    '-x^4-2x^2+17',      '-2x^5+x^3-x^4-x-19',   '-x^2+x^4-x^5-25',
-    'x+x^4-x^5-17',      '-2x^3+x^5-9',          '-2x^4+x^3-24',
-    '-2x^4+x^5+x^2-29',  '-x^3+x-x^5-18',        'x^4-x^2-x-17',
-    '-x^3+x^4-x^5+10',   '-x^2+x^4-x^3+25',      '-x^3-x^5-19',
-    '-3x^2+x^3+x-25',    '2x^2-3x^4-17',         '-2x^4+x^2-x^3-x-29',
-    '2x^5-x^4-11',        '-x+x^4-x^3-17',       '-2x-2x^2+x^4-29',
-    '-x^5+x^4-16',        '-2x^3+x^2-x^5-24',    '-x^2-x^4+x^3-4',
-    'x^3-2x^2+22',        '-2x^4+x^5+36',        '3x^3-x^2+31',
-    '-x^2+x^4-x^5-24',    '-2x^2-x^4-15',        '-x^2-x^3-19',
-    '-2x+x^4+x^2-23',     '2x-x^3-x^2-x^5-5',    '-2x^2+x^4-x^3+x-8',
-    '2x^5-x^3-13',        '-x^2-x^3-x^4-20',     '2x^4+x^2-x-27',
-    '-3x+x^5+x^2-32',     '-2x^2+x^5+x-x^4-24',  '-2x^2+x^4-x-x^5-16',
-    'x-x^4-x^5+21',       '2x^2-2x^4-x^3-18',    '2x^2-2x^5-x-13',
-    '2x^4-2x^5-x^2-12',   '-x^2-x^4-x-19',       '-2x^2+2x^3+x^5-18',
-    '-2x+x^4+x^2-x^3-13', '2x^2+x^4-x^3-22',     '-2x^3-x^5-11',
-    '-2x^3+x^2-26',       '-2x^3-x^4-7',         '-2x^5+x^2-29',
-    '-x^4+x^2-x-2',       '-2x^2-x^4-3',         '-2x^3+x^4-32',
-    '-2x+x^5-x^3-18',     '2x^4+x^2-x-16',       'x^3-x^5-x^4-5',
-    '-x+x^5-x^2-10',      'x^5-x-x^3-31',        '-2x^5+x^4-x^2-27',
-    '-2x^2-x^4-18',       'x^3-x^2-x^4-7',       '-2x^2-x^3+16',
-    '-2x^4+x^5-14',       '-2x^2+x^3-24',        '-2x^2+x^3+31',
-    '-x^4-x^5-11',        '-2x^4+x^2-22',        '-2x^3+x^2-21',
-    '-x+x^2-x^3-25'
-  ];
 
 //
 // MAIN FUNCTIONS
@@ -838,8 +772,14 @@ function compareDerivatives(userAnswer, func) {
 }
 
 function compareCalculations(userAnswer, func, value) {
-    const correctCalculation = calculate(func, value);
-    if (userAnswer == correctCalculation) {
+    const correctCalculation = simplify(calculate(func, value));
+    const correctCalculationFractional = correctCalculation.simplifiedFractional;
+    const correctCalculationDecimal = correctCalculation.simplifiedDecimal;
+    const userAnswerSimplified = simplify(userAnswer);
+    const userAnswerFractional = userAnswerSimplified.simplifiedFractional;
+    const userAnswerDecimal = userAnswerSimplified.simplifiedDecimal;
+    if (correctCalculationDecimal == userAnswerDecimal ||
+        correctCalculationFractional == userAnswerFractional) {
         return true;
     } else return false;
 }
@@ -945,6 +885,8 @@ function processExpressionForCompare(expression) {
     return xExpression;
 }
 
+
+console.log(compareCalculations('40/4 + 1 - 1', '4x^2 - 6', '2'))
 
 const _derivativeOf = derivativeOf;
 export { _derivativeOf as derivativeOf };
